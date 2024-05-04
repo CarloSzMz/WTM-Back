1b2e2fe6f987427b64149dcaec5198051c4fff31
Carlos Sanz <102948452+CarloSzMz@users.noreply.github.com>
2024-05-05 00:06:20 +0200

    cambio de precio a double

--- database/migrations/crear_stock.php
+++ database/migrations/crear_stock.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->integer('quantity');
-            $table->integer('price');
+            $table->double('price');
             $table->timestamps();
         });
     }
