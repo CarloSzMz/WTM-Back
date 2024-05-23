4ac7ca467680ff401e893d4ad2a498a4cbdf4fc3
Carlos Sanz <102948452+CarloSzMz@users.noreply.github.com>
2024-05-23 23:38:13 +0200

    cambio estilos

--- resources/views/layout/styles.blade.php
+++ resources/views/layout/styles.blade.php
@@ -18,10 +18,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
-    <link href="{{ asset('assets/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
     <!-- Using CDN -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
-        integrity="sha512-...." crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.6/css/jquery.dataTables.css">
 
     <!--Icons-->
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,12 +1,37 @@
 @extends('layout.main')
 @section('content')
+    <style>
+        @media only screen and (max-width: 961px) {
+            #ordersByMonthChart {
+                max-width: 250px;
+                max-height: 500px;
+            }
+        }
+
+        .dataTables_wrapper {
+            margin-top: 20px;
+            /* Agrega margen en la parte superior de los elementos de DataTables */
+        }
+
+        .dataTables_wrapper .dataTables_filter {
+            margin-bottom: 20px;
+        }
+
+        .dt-layout-row {
+            padding-bottom: 20px;
+        }
+
+        .dt-length {
+            display: none;
+        }
+    </style>
     <div>
         <div class="bg-light m-5 shadow">
             <div class="w-100 bg-secondary p-2 text-light">
                 <h2> Usuarios </h2>
             </div>
-            <div class="mt-2 p-3">
-                <table class="table table-striped table-dark">
+            <div class="mt-2 p-3 table-responsive">
+                <table class="table table-striped table-dark" id="userTable">
                     <thead class="thead-dark">
                         <th>ID</th>
                         <th>Nombre</th>
@@ -45,10 +70,11 @@
                             </tr>
                         @endforeach
                     </tbody>
+                    <button class="btn btn-primary float-end"><a href={{ route('users.create') }}
+                            class="text-decoration-none text-light">Crear Usuario</a></button>
+
                 </table>
 
-                <button class="btn btn-primary"><a href={{ route('users.create') }}
-                        class="text-decoration-none text-light">Crear Usuario</a></button>
             </div>
 
 
@@ -58,8 +84,8 @@
         <div class="w-100 bg-secondary p-2 text-light">
             <h2> Direcciones </h2>
         </div>
-        <div class="mt-2 p-3">
-            <table class="table table-striped table-dark">
+        <div class="mt-2 p-3 table-responsive">
+            <table class="table table-striped table-dark" id="direccionestable">
                 <thead class="thead-dark">
                     <th>ID</th>
                     <th>Nombre</th>
@@ -79,4 +105,21 @@
             </table>
         </div>
     </div>
+
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <script type="text/javascript" src="https://cdn.datatables.net/2.0.7/js/dataTables.min.js"></script>
+    <!--DataTable Implementation-->
+    <script>
+        $(document).ready(function() {
+            $('#userTable').DataTable({
+                "paging": true,
+                "pageLength": 7
+            });
+            $('#direccionestable').DataTable({
+                "paging": true,
+                "pageLength": 7
+            });
+        });
+    </script>
 @endsection
